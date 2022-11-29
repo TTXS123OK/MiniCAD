089231f8b3c808083f97598730a08cfbac705f5e
TTXS123OK <874238092@qq.com>
2022-11-29 20:54:36 +0800

    fix bug drawing rectangle

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -9,10 +9,9 @@ public class Rectangle extends Shape {
     private int height;
 
     public Rectangle(Point p1, Point p2) {
-        assert (p1.x < p2.x && p1.y < p2.y);
-        this.point = p1;
-        width = p2.x - p1.x;
-        height = p2.y - p1.y;
+        this.point = new Point(Math.min(p1.x, p2.x), Math.min(p1.y, p2.y));
+        width = Math.abs(p2.x - p1.x);
+        height = Math.abs(p2.y - p1.y);
     }
 
     public void render(Graphics2D g) {
