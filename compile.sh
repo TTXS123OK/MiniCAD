bc67c75cd2dde917f01d82e50fbc84374e7985a9
TTXS123OK <874238092@qq.com>
2022-11-30 15:24:31 +0800

    modify compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,20 @@
 # compile source file to classes
+echo "compiling source file begins...";
 javac -cp ./src/ -d ./bin/ ./src/Main.java;
+echo "compile finished...";
 
 # copy manifest to bin
+echo "coping manifest...";
 cp -r ./src/META-INF ./bin/;
 
 # exit if cd fails
 cd ./bin || exit;
 
 # package classes to jar
-jar cmvf ./META-INF/MANIFEST.MF MiniCAD.jar .;
+echo "packaging classes files to jar...";
+jar cmvf ./META-INF/MANIFEST.MF MiniCAD.jar . > /dev/null;
 
 # remove intermediate compilation files
+echo "cleaning intermediate files...";
 find . ! -name "MiniCAD.jar" ! -name "." ! -name ".." -type f,d -exec rm -rf {} +;
+echo "project files packaged to ./bin/MiniCAD.jar";
